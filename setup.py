5ea3a3935331695fef84515d1e4a5bef6696cf73
laurenrwolf <86804000+laurenrwolf@users.noreply.github.com>
2022-07-08 14:07:58 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyze Upward Mobility metrics, resulting in insightful comparisons and informative visualizations',
+    author='Team Upward Mobility',
+    license='',
+)
